6d9c2d6404176be54f8e32ac13ef1bb70fe1197f
rishab.batra <relianceJio@dev.azure.com>
2023-05-19 12:16:58 +0530

    ChatGPTIntegration

---
+++ src/main/java/org/example/ChatGPTIntegration.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.*;
+import java.io.IOException;
+
+public class ChatGPTIntegration {
+    private static final String OPENAI_API_KEY = "YOUR_API_KEY";
+    private static final String OPENAI_API_URL = "https://api.openai.com/v1/engines/davinci-codex/completions";
+
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        // Compose the API request payload
+        String inputPrompt = "What is the meaning of life?";
+        String payload = "{\"prompt\": \"" + inputPrompt + "\", \"max_tokens\": 50}";
+
+        // Create the HTTP request
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"), payload);
+        Request request = new Request.Builder()
+                .url(OPENAI_API_URL)
+                .post(body)
+                .addHeader("Authorization", "Bearer " + OPENAI_API_KEY)
+                .build();
+
+        // Send the request and retrieve the response
+        Response response = client.newCall(request).execute();
+        String responseBody = response.body().string();
+
+        // Process the response
+        // Assuming you have a JSON parsing library like Jackson or Gson, you can parse the response JSON
+        // and extract the generated text from the 'choices' field
+        // For example, using Jackson:
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(responseBody);
+        String generatedText = rootNode.get("choices").get(0).get("text").asText();
+
+        System.out.println("Generated Text: " + generatedText);
+    }
+}
